f4f05a05c08c8c46ea517a3a7bfbb158274735e6
Jose Ramirez <jose@firstfactory.com>
2014-05-19 08:19:59 -0600

    remove unnecessary code

--- angular_webApp/src/app/home/home.ctrl.js
+++ angular_webApp/src/app/home/home.ctrl.js
@@ -8,30 +8,6 @@ app.controller('HomeController', function ($scope,$rootScope,$route) {
 
         angular.element('#easy-pie-chart-2').easyPieChart({});
         angular.element('#c-tooltips button').tooltip();
-        angular.element('.sparkline-bar').sparkline('html', {
-            type: 'bar',
-            width:'30px',
-            height:'25px'
-        });
-//
-//        Morris.Line({
-//            element: 'line-example',
-//            data: [
-//                { y: '2006', a: 100, b: 90 },
-//                { y: '2007', a: 75,  b: 65 },
-//                { y: '2008', a: 50,  b: 40 },
-//                { y: '2009', a: 75,  b: 65 },
-//                { y: '2010', a: 50,  b: 40 },
-//                { y: '2011', a: 75,  b: 65 },
-//                { y: '2012', a: 100, b: 90 }
-//            ],
-//            gridLineColor: 'rgba(255,255,255,.5)',
-//            xkey: 'y',
-//            ykeys: ['a', 'b'],
-//            labels: ['Series A', 'Series B']
-//        });
-
-
 
         var uploads_data = [
             { day: '2014-03-10', v: 20 },
@@ -42,10 +18,8 @@ app.controller('HomeController', function ($scope,$rootScope,$route) {
             { day: '2014-03-15', v: 5  },
             { day: '2014-03-16', v: 20 }
         ];
-
-
-        Morris.pixelSparkline({
-            element: 'line-example',
+        Morris.Line({
+            element: 'hero-graph',
             data: uploads_data,
             xkey: 'day',
             ykeys: ['v'],
