045c82cfa21f05826aafc56e3b3b2369eabd2a4a
Jose Ramirez <jose@firstfactory.com>
2014-05-19 08:26:22 -0600

    Fix some issues after conflict.

--- angular_webApp/src/app/app.js
+++ angular_webApp/src/app/app.js
@@ -1,19 +1,7 @@
 'use strict';
 (function(app) {
     app.config(function ($routeProvider) {
-        $routeProvider.when('/home', {templateUrl: 'app/home/home.tpl.html', resolve:{deps:function($q, $rootScope){
-            var deferred = $q.defer();
-            var essentials = [
-                'app/home/home.ctrl.js'
-            ];
-            $script(essentials,function(){
-                    // all dependencies have now been loaded by $script.js so resolve the promise
-                    $rootScope.$apply(function() {
-                        deferred.resolve();
-                    });
-            });
-            return deferred.promise;
-        }}});
+        $routeProvider.otherwise({redirectTo: '/home'});
     });
     app.run(function () {});
 }(angular.module("gRockitApp", [
