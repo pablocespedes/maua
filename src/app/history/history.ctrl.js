82b145058b643b63aff51c008baa6a1fdc777ec1
Jose Andres Ramirez Mora <jose@firstfactory.com>
2014-09-25 13:23:47 -0600

    Add static question Id for testing purposes. Fix history url to a valid url. Clean history controller.

--- src/app/history/history.ctrl.js
+++ src/app/history/history.ctrl.js
@@ -11,24 +11,10 @@
     // dummy data for pagination
     var self = this;
     HistoryApi.query({ groupId: 'gmat' }).$promise.then(function(data) {
-      console.log(data);
       self.questionsPerDay = data;
     });
     this.currentPage = 1;
     this.pageChanged = function() {
-    };
-
-    var observable = Observable.create('test'),
-        gottenObservable = Observable.get('test'),
-        observer2 = gottenObservable.register(function(data) {
-          console.log('2: ' + data);
-        }),
-        observer1 = gottenObservable.register(function(data) {
-          console.log('1: ' + data);
-        });
-
-    this.testClick = function(title) {
-      observable.notify(title);
     };
 
   }
