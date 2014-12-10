1cadca2329879a8c511c76198a13b1bd48279099
Jose Andres Ramirez Mora <jose@firstfactory.com>
2014-12-09 23:33:15 -0600

    Init commit for new Dashboard UI.

--- src/app/components/dashboard/dashboard.directive.js
+++ src/app/components/dashboard/dashboard.directive.js
@@ -25,6 +25,54 @@
     return directive;
 
     function link(scope, element, attrs) {
+
+              var collection = [{name:'Absolute value', percent: '80%', perctCss:'percent_80'},
+                  {name:'Algebra', percent: '30%', perctCss:'percent_30'},
+                  {name:'Angles', percent: '10%', perctCss:'percent_10'},
+                  {name:'Area', percent: '20%', perctCss:'percent_20'},
+                  {name:'Arithmetic', percent: '45%', perctCss:'percent_40'},
+                  {name:'Averages', percent: '52%', perctCss:'percent_50'},
+                  {name:'Circles', percent: '14%', perctCss:'percent_10'},
+                  {name:'Coordinate geometry', percent: '19%', perctCss:'percent_10'},
+                  {name:'Data Sufficiency', percent: '35%', perctCss:'percent_30'},
+                  {name:'Decimals', percent: '30%', perctCss:'percent_10'},
+                  {name:'Distance problem', percent: '30%', perctCss:'percent_10'},
+                  {name:'Estimation', percent: '30%', perctCss:'percent_10'},
+                  {name:'Evaluating expressions', percent: '19%', perctCss:'percent_10'},
+                  {name:'Exponents and Roots', percent: '25%', perctCss:'percent_20'},
+                  {name:'Factors, Divisibility and Prime Numbers', percent: '5%', perctCss:'percent_0'},
+                  {name:'Fractions', percent: '30%', perctCss:'percent_10'},
+                  {name:'Functions', percent: '30%', perctCss:'percent_10'},
+                  {name:'Geometry', percent: '30%', perctCss:'percent_10'},
+                  {name:'Inequalities', percent: '30%', perctCss:'percent_10'},
+                  {name:'Inscribed Figures', percent: '30%', perctCss:'percent_10'},
+                  {name:'Interest', percent: '30%', perctCss:'percent_10'},
+                  {name:'Interpretation of graphs and tables', percent: '30%', perctCss:'percent_10'},
+                  {name:'Intersecting lines and angles', percent: '30%', perctCss:'percent_10'},
+                  {name:'Linear Equations with 1 Variable', percent: '30%', perctCss:'percent_10'},
+                  {name:'Lines', percent: '30%', perctCss:'percent_10'},
+                  {name:'Number Properties', percent: '30%', perctCss:'percent_10'},
+                  {name:'Parallel Lines', percent: '30%', perctCss:'percent_10'},
+                  {name:'Percents', percent: '30%', perctCss:'percent_10'},
+                  {name:'Perimeter', percent: '30%', perctCss:'percent_10'},
+                  {name:'Probability: Combinations, Permutations and Counting', percent: '30%', perctCss:'percent_10'},
+                  {name:'Problem Solving', percent: '30%', perctCss:'percent_10'},
+                  {name:'Profit', percent: '30%', perctCss:'percent_10'},
+                  {name:'Quadratic Equations', percent: '30%', perctCss:'percent_10'},
+                  {name:'Quadrilaterals', percent: '30%', perctCss:'percent_10'},
+                  {name:'Rates and Work', percent: '30%', perctCss:'percent_10'},
+                  {name:'Ratios and proportions', percent: '30%', perctCss:'percent_10'},
+                  {name:'Sequences', percent: '30%', perctCss:'percent_10'},
+                  {name:'Sets', percent: '30%', perctCss:'percent_10'},
+                  {name:'Simplifying algebraic expressions', percent: '30%', perctCss:'percent_10'},
+                  {name:'Statistics', percent: '30%', perctCss:'percent_10'},
+                  {name:'System of Linear Equations', percent: '30%', perctCss:'percent_10'},
+                  {name:'Triangles'},{name:'Volume', percent: '30%', perctCss:'percent_10'},
+                  {name:'Word Problems', percent: '30%', perctCss:'percent_10'}];
+
+      scope.trackTags= collection;
+
+
       var overlayTrack = null;
       scope.shouldShowOverlay = function(track) {
         if (track.hasScore) {
